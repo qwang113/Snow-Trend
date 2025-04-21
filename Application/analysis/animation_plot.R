009b1ef714be20be982415292395834d8c7e3f75
Qi Wang <72365648+qwang-77@users.noreply.github.com>
2025-04-21 07:46:06 -0700

    update

--- Application/analysis/animation_plot.R
+++ Application/analysis/animation_plot.R
@@ -144,10 +144,10 @@ for (i in 2:52) {
   #       barheight = 0.5  # Adjust the height of the color bar
   #     )
   #   )
-  trend_aeqd_lat_alt$color_val <- pmax(pmin(trend_aeqd_lat_alt[[i]], 0.3), -0.3)
+  trend_aeqd_without_lat_alt$color_val <- pmax(pmin(trend_aeqd_without_lat_alt[[i]], 0.5), -0.5)
   plot3 <- ggplot() +
     geom_sf(data = world_aeqd, fill = "lightgray", color = NA) +
-    geom_sf(data = trend_aeqd_lat_alt, aes(color = color_val), size = 4, shape = 18) +
+    geom_sf(data = trend_aeqd_without_lat_alt, aes(color = color_val), size = 4, shape = 18) +
     geom_sf(data = world_aeqd, fill = NA, color = "black") +
     geom_sf(data = equator_aeqd, color = "red", linetype = "dashed", size = 0.1) +
     scale_color_gradient2(
@@ -155,7 +155,7 @@ for (i in 2:52) {
       mid = "white",       
       high = "blue",        
       midpoint = 0,
-      limits = c(-0.3, 0.3),
+      limits = c(-0.5, 0.5),
       guide = "colourbar"
     ) +
     theme_minimal() +
@@ -259,16 +259,16 @@ for (i in 2:52) {
   )
 }
 
-setwd("D:/77/Research/temp/snow/frame_by_frame")
-png_files <- list.files(pattern = "plot_\\d+\\.png") 
+setwd("D:/77/Research/temp/snow")
+png_files <- list.files(pattern = "plot_\\d+\\.png$") 
 png_files <- png_files[order(as.numeric(gsub("\\D", "", png_files)))]# Find all saved PNGs
 gif <- image_read(png_files)                       # Read images
 gif <- image_animate(gif, fps = 5)                 # Set frames per second (5 fps)
-image_write(gif, "trend_animation.gif")            # Save the GIF
+image_write(gif, "trend_animation_bym.gif")            # Save the GIF
 
 special <- c( which(wk_names %in% c("11.27", "1.15","4.1","6.3")))
 
-trend_aeqd_lat_alt$color_val <- pmax(pmin(trend_aeqd_without_lat_alt[[special[1]]], 0.3), -0.3)
+trend_aeqd_lat_alt$color_val <- pmax(pmin(trend_aeqd_lat_alt[[special[1]]], 0.3), -0.3)
 p1 <- ggplot() +
   geom_sf(data = world_aeqd, fill = "lightgray", color = NA) +
   geom_sf(data = trend_aeqd_lat_alt, aes(color = color_val), size = 4, shape = 18) +
@@ -397,6 +397,3 @@ p4 <- ggplot() +
 
 cowplot::plot_grid(p1,p2, nrow = 1)
 cowplot::plot_grid(p3,p4, nrow = 1)
-
-thetas <- readRDS("D:/77/Research/temp/snow/theta01_bym.Rda")
-taus <- readRDS("D:/77/Research/temp/snow/tau01_bym.Rda")
