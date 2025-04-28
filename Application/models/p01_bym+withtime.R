3005cc6b580f3dfe5cad906816d8822854c8720c
Qi Wang <1061673783@qq.com>
2025-04-28 09:53:20 -0700

    a

--- Application/models/p01_bym+withtime.R
+++ Application/models/p01_bym+withtime.R
@@ -174,5 +174,5 @@ while(save_idx < tot_samples) {
 }
 
 setwd("D:/77/Research/temp/snow/")
-saveRDS(all_theta, "theta01_bym+notime.Rda")
-saveRDS(all_tau, "tau01_bym+notime.Rda")
+saveRDS(all_theta, "theta01_bym+withtime.Rda")
+saveRDS(all_tau, "tau01_bym+withtime.Rda")
