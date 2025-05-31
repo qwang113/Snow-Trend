8535a5bc000638b05387273fd97ce540bd3eb891
Qi Wang <1061673783@qq.com>
2025-05-31 10:29:48 -0700

    update data preprocessing, enhance the correct data

--- check_data.R
+++ check_data.R
@@ -4,7 +4,7 @@ colnames(dat_yisu)
 dates <- as.Date(sub("^X", "", colnames(dat_yisu)[-c(1,2)]), format = "%m.%d.%Y")
 colnames(dat_yisu)[-c(1,2)] <- as.character(dates)
 
-full_dat <- readRDS(here::here("snow_cleaned_full.Rda"))
+full_dat <- readRDS(here::here("C:/Users/lix23/Desktop/Snow-Trend/snow_cleaned_full.Rda"))
 
 full_selected <- full_dat[,colnames(dat_yisu)[263:ncol(dat_yisu)]]
 yisu_selected <- dat_yisu[,263:ncol(dat_yisu)]
